063d60530285bf474398e3f274ee82e845e78e93
Sourcery AI <>
2023-11-04 02:38:26 +0000

    'Refactored by Sourcery'

--- demo01/bshr_loop.py
+++ demo01/bshr_loop.py
@@ -5,32 +5,26 @@ import json
 
 
 def bshr_loop(query):
-    all_search_results = list()  # this should be a list of dicts with something like "query", "source", and "content" 
-    all_hypotheses = list()  # this should be a list of strings
+    all_search_results = []
+    all_hypotheses = []
     while True:
         ## STEP 1: BRAINSTORM
         search_queries = generate_search_queries(query, all_search_results, all_hypotheses)  # will generate new search queries based upon main query, all results, and all hypotheses)
         # TODO write generate_search_queries function
-        
+
         ## STEP 2: SEARCH
         new_search_results = execute_searches(search_queries)  # will retrieve more information from assigned data source
         # TODO write execute_searches function
         # TODO update all_search_results object with new results
-        
+
         ## STEP 3: HYPOTHESIZE
         new_hypothesis = generate_new_hypothesis(query, all_search_results, all_hypotheses)  # will generate new and improved hypothesis based on all available data
-        # TODO write generate_new_hypothesis function
-        # TODO update all_hypotheses object
-        
-        ## STEP 4: REFINE
-        satisficed = check_satisficed(query, all_search_results, all_hypotheses)  # test if the main query has been satisficed 
-        # TODO write check_satisficed function
-        if satisficed:
+        if satisficed := check_satisficed(
+            query, all_search_results, all_hypotheses
+        ):
             return all_search_results, all_hypotheses
-        
-        exhausted = check_exhausted(query, all_search_results)  # test if we seem to have exhausted all available information
-        # TODO write check_exhausted function
-        if exhausted:
+
+        if exhausted := check_exhausted(query, all_search_results):
             return all_search_results, all_hypotheses
         
         ## if neither test is satisfied, the loop will recurse, repeating the cycle of brainstorming, searching, and hypothesizing, thus further refining the answer.
--- demo01/src/system02_hypothesize.py
+++ demo01/src/system02_hypothesize.py
@@ -3,8 +3,8 @@ from utils import use_chatgpt, get_system_message
 def hypothesize(user_query: str, notes: str, hypotheses: str):
 
     system_message = get_system_message('system02_hypothesize.txt')
-    user_message = (
-f"""
+        user_message = (
+    f"""
 # USER QUERY
 {user_query}
 
@@ -16,9 +16,9 @@ f"""
 # PREVIOUS HYPOTHISES
 {hypotheses}
 """
-    )
+        )
     response, tokens = use_chatgpt(system_message, user_message)
-    print("new hypothesis: " + response)
+    print(f"new hypothesis: {response}")
 
     return response, tokens
 
